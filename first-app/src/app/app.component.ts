b330636fa6895cb6ae8267e605ec5ae083b47fa9
Tolala9 <anatolii.lukavenko@gmail.com>
2019-04-05 23:49:23 +0300

    Practice task directives write with timestamp

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showPassword = false;
+	showPassword = false;
 	detailsDateArr = [];
-	// countDateLogItems = 0;
-
+	
 	onCreatePassword() {
-  	// this.detailsDateArr.push(new Date().toString());
-  	this.detailsDateArr.push(this.detailsDateArr.length + 1);
-  }
+		const item = {
+			index: this.detailsDateArr.length,
+			date: new Date()
+		}
+		this.detailsDateArr.push(item);
+	}
 }
