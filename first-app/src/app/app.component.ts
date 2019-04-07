072bcca6099f8649e85fa8373e62ae83180ef511
Tolala9 <anatolii.lukavenko@gmail.com>
2019-04-07 23:25:59 +0300

    Createed app structure by components

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -6,11 +6,5 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	showPassword = false;
-	detailsDateArr = [];
 	
-	onCreatePassword() {
-		
-		this.detailsDateArr.push( new Date() );
-	}
 }
